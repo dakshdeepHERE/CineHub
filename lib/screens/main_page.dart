9f17fb53ab33f834a80a0b9e996f407dbbc443d3
Daksha Deep <dakshadeep1234@gmail.com>
2023-07-12 15:55:35 +0530

    few more changes

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,6 +1,3 @@
-// ignore_for_file: use_build_context_synchronously
-
-import 'package:movie_app/cubit/app_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
